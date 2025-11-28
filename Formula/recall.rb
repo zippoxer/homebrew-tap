da08c5a1d98f47b403b6be06a08e939b06048a17
zippoxer <moshe@zippo.io>
2025-11-28 03:46:21 +0200

    Update recall to v0.1.0 with proper SHA256 hashes

--- Formula/recall.rb
+++ Formula/recall.rb
@@ -7,18 +7,18 @@ class Recall < Formula
   on_macos do
     on_intel do
       url "https://github.com/zippoxer/recall/releases/download/v#{version}/recall-macos-intel.tar.gz"
-      sha256 "PLACEHOLDER_INTEL_SHA256"
+      sha256 "46c3360536b86a5fbf3570460141769b3492c9d3ae6d2c693bb85cd2404f14c4"
     end
 
     on_arm do
       url "https://github.com/zippoxer/recall/releases/download/v#{version}/recall-macos-arm64.tar.gz"
-      sha256 "PLACEHOLDER_ARM64_SHA256"
+      sha256 "9c8c167d8d625adf847824dc4b56c40f41cd0e19ccf0524defd13023f03c21a6"
     end
   end
 
   on_linux do
     url "https://github.com/zippoxer/recall/releases/download/v#{version}/recall-linux-x86_64.tar.gz"
-    sha256 "PLACEHOLDER_LINUX_SHA256"
+    sha256 "b7487783ecf5350718a7217350794eecc90d4bd27f6654fafc9a02e1b91101d2"
   end
 
   def install
