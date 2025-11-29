995ce7e5346a7ee0553e1aecd1605feef75898cc
zippoxer <moshe@zippo.io>
2025-11-29 19:26:16 +0200

    Update recall to v0.1.1

--- Formula/recall.rb
+++ Formula/recall.rb
@@ -1,24 +1,24 @@
 class Recall < Formula
   desc "Search and resume your Claude Code and Codex CLI conversations"
   homepage "https://github.com/zippoxer/recall"
-  version "0.1.0"
+  version "0.1.1"
   license "MIT"
 
   on_macos do
     on_intel do
       url "https://github.com/zippoxer/recall/releases/download/v#{version}/recall-macos-intel.tar.gz"
-      sha256 "e5a3e81e7f19785de2a370895d345ca010d51d85105b71f9fe6cf6ebba8ce7cf"
+      sha256 "98f2db4380d81fe7a5c0d6bbbe46de36374719f3f120588cb17b7c58e13940b8"
     end
 
     on_arm do
       url "https://github.com/zippoxer/recall/releases/download/v#{version}/recall-macos-arm64.tar.gz"
-      sha256 "874fe3619ac207faa2362cc3ea9fe2e9121239e261189fcf64817c50a12780cd"
+      sha256 "7b5ec97e75df6eb901e030295a2d51c9b23e84635b8056fd470d3495056e7586"
     end
   end
 
   on_linux do
     url "https://github.com/zippoxer/recall/releases/download/v#{version}/recall-linux-x86_64.tar.gz"
-    sha256 "df752eb0ccfe3b5a2fe14db1e453364b6477271381f5a7a8a1778758ff9791af"
+    sha256 "92a5b6ee91a7b06030cf2200a6df88d25c042f470024c0274326e4f6d9ccfc01"
   end
 
   def install
